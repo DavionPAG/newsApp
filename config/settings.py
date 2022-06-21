61ff16e083d6dbe41f8ffb8cc20bd68bb5abbf3e
Davion <daviongarcia@gmail.com>
2022-06-20 18:07:06 -0700

    heroku push

--- config/settings.py
+++ config/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': env.dj_db_url('DATABASE_URL')
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': env.dj_db_url('DATABASE_URL')
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
     #test23
 }
 
